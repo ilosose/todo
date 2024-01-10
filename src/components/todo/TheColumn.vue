d6f32da4eb2172765ac86cb99fad3dc3c6794dcd
Azazel <chepelev007@gmail.com>
2024-01-10 14:38:45 +0300

    [ID14-theModal-style]добавил стили на кнопки

--- src/components/todo/TheColumn.vue
+++ src/components/todo/TheColumn.vue
@@ -1,111 +1,122 @@
 <template>
-   <div class="kanban" v-if="columns">
+  <div class="kanban" v-if="columns">
     <section
-      v-for="status in columns" 
+      v-for="status in columns"
       :key="status.id"
       class="kanban__column"
       @dragover.prevent="allowDrop"
-      @drop="handleDrop" 
+      @drop="handleDrop"
     >
       <div class="kanban__header">
         <div class="kanban__header-content">
           <h2 class="kanban__title">{{ status.status.name }}</h2>
-        </div>      
-        <img 
-          src="../assets/img/kanban/plus.svg" 
-          alt="Добавить задачу" 
+        </div>
+        <div class="icons">
+        <img
+          src="../assets/img/kanban/pen.svg"
+          alt="Изменить колонку"
+          class="kanban__icon--edit"
+        />
+        <img
+          src="../assets/img/kanban/plus.svg"
+          alt="Добавить задачу"
           class="kanban__icon kanban__icon--add"
-          @click="$emit('add-task', status.status.id)">
+          @click="$emit('add-task', status.status.id)"
+        />
+      </div>
       </div>
       <div class="kanban__list">
-        <kanban-task 
-          v-for="task in status.tasks"
-          :key="task.id"
-          :task="task"  
-        />
+        <kanban-task v-for="task in status.tasks" :key="task.id" :task="task" />
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import KanbanTask from './TheTask.vue';
-import { mapGetters } from 'vuex';
+import KanbanTask from "./TheTask.vue";
+import { mapGetters } from "vuex";
 
-  export default {
-    components: {
-      KanbanTask,
+export default {
+  components: {
+    KanbanTask,
+  },
+
+  computed: {
+    ...mapGetters("columns", ["columns"]),
+  },
+
+  methods: {
+    allowDrop(event) {
+      event.preventDefault();
     },
 
-    computed: {
-      ...mapGetters('columns', ['columns'])
+    handleDrop(event) {
+      event.preventDefault();
+      const taskId = event.dataTransfer.getData("text/plain");
+      this.$emit("task-droped", Number(taskId), this.column.id);
     },
-
-    methods: {
-      allowDrop(event) {
-        event.preventDefault();
-      },
-      
-      handleDrop(event) {
-        event.preventDefault();
-        const taskId = event.dataTransfer.getData('text/plain');
-        this.$emit('task-droped', Number(taskId), this.column.id);
-      },
-    },
-  }
+  },
+};
 </script>
 
 <style scoped>
-  .kanban {
-    display: flex;
-    padding: 40px;
-    align-items: flex-start;
-    gap: 40px;
-    margin: 10px 0;
-  }
+.kanban__icon--edit {
+  
+  cursor: pointer;
+  padding-right: 8px;
+}
+.kanban__icon--edit:hover{
+  opacity: 0.7;
+}
+.kanban {
+  display: flex;
+  padding: 40px;
+  align-items: flex-start;
+  gap: 40px;
+  margin: 10px 0;
+}
 
-  .kanban__column {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 24px;
-    flex: 1 0 0;
-    padding: 14px;
-    border-radius: 12px;
-    background-color: #D5CCFF;
-  }
-  .kanban__header {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      width: 100%;
-  }
+.kanban__column {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 24px;
+  flex: 1 0 0;
+  padding: 14px;
+  border-radius: 12px;
+  background-color: #d5ccff;
+}
+.kanban__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+}
 
-  .kanban__header-content {
-      display: flex;
-      align-items: center;
-      gap: 10px;
+.kanban__header-content {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
 
-  }
+.kanban__title {
+  color: #2b1887;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+}
 
-  .kanban__title {
-      color: #2B1887;
-      font-size: 16px;
-      font-style: normal;
-      font-weight: 600;
-      line-height: normal;
-  }
+.kanban__icon--add:hover {
+  cursor: pointer;
+  opacity: 0.5;
+}
 
-  .kanban__icon--add:hover {
-      cursor: pointer;
-      opacity: .5;
-  }
-
-  .kanban__list {
-      display: flex;
-      flex-direction: column;
-      gap: 10px;
-      width: 100%;
-  }
+.kanban__list {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+}
 </style>
